dba730ec5b713112f7b61940ea891aac361807d6
Pierre GIRAUDEAU <pgiraude@made-f0Ar7s10.clusters.42paris.fr>
2024-10-17 20:13:28 +0200

    d03 ex03

--- d03/ex03/roads_to_philosophy.py
+++ d03/ex03/roads_to_philosophy.py
@@ -1,16 +1,59 @@
-import modules
 import sys
 import requests
-import BeautifulSoup
+from bs4 import BeautifulSoup
 
-def roads_to_philosophy(path):
-    wiki_request = requests.get(url="https://en.wikipedia.org/wiki/{path}", params=PARAMS)
+class  roads_to_philosophy:
+    def __init__(self):
+        self.wikiTitles = []
+
+    def get_wiki_title(self, title):
+        URL = "https://en.wikipedia.org/wiki/{title}".format(title=title)
+
+        try:
+            requestWiki = requests.get(url=URL)
+            requestWiki.raise_for_status()
+        except requests.HTTPError as error:
+            if requestWiki.status_code == 404:
+                return print("It's a dead end !")
+            return print(error)
+
+        soup = BeautifulSoup(requestWiki.text, "html.parser")
+        newTitle = soup.find('h1').text
+        if newTitle == 'Philosophy':
+            return gfhfghf
+        if newTitle in self.wikiTitles:
+            return print("It leads to an infinite loop !")
+        self.wikiTitles.append(newTitle)
+
+        content = soup.find(id="mw-content-text")
+        links = content.find_all('a')
+
+        hrefs = [link.get('href') for link in links]
+        filteredHrefs = [href for href in hrefs if href and href.startswith('/wiki/')
+            and not href.startswith('/wiki/Wikipedia:') and not href.startswith('/wiki/Help:')
+            and not href.startswith('/wiki/File:')]
+
+        print(filteredHrefs)
+    
+
+
+    
+
+# def check_wiki_title(title):
+#     wikiTitles = []
+#     newTitle = get_wiki_title(title)
+#     if newTitle == 'Philosophy':
+#         return print("{number} roads from {title} to Philosophy".format(number=len(wikiTitles), title=wikiTitles[0] if len(wikiTitles) > 0 else 'Philosophy'))
+
+    
 
 
 def main():
-    if sys.argv != 2
+    if len(sys.argv) != 2:
+        print("Need one argument")
         return
-    def 
+    test = roads_to_philosophy()
+    test.get_wiki_title(sys.argv[1])
 
 if __name__ == '__main__':
     main()
