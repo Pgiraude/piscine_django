bf8f5eee6a05ca3c4b80f6307831fbef6ae49715
Pierre GIRAUDEAU <pgiraude@made-f0Ar7s10.clusters.42paris.fr>
2024-10-18 16:15:08 +0200

    d03

--- d03/ex03/roads_to_philosophy.py
+++ d03/ex03/roads_to_philosophy.py
@@ -7,7 +7,7 @@ class  roads_to_philosophy:
         self.wikiTitles = []
 
     def get_wiki_title(self, title):
-        URL = "https://en.wikipedia.org/wiki/{title}".format(title=title)
+        URL = "https://en.wikipedia.org{title}".format(title=title)
 
         try:
             requestWiki = requests.get(url=URL)
@@ -19,41 +19,29 @@ class  roads_to_philosophy:
 
         soup = BeautifulSoup(requestWiki.text, "html.parser")
         newTitle = soup.find('h1').text
+        print(newTitle)
         if newTitle == 'Philosophy':
-            return gfhfghf
+            return print("{number} roads from {title} to Philosophy".format(number=len(self.wikiTitles), title=self.wikiTitles[0] if len(self.wikiTitles) > 0 else 'Philosophy'))
         if newTitle in self.wikiTitles:
             return print("It leads to an infinite loop !")
         self.wikiTitles.append(newTitle)
 
         content = soup.find(id="mw-content-text")
         links = content.find_all('a')
-
-        hrefs = [link.get('href') for link in links]
+        hrefs = [link.get('href') for link in links if link.find_parent('p')
+            and not link.find_parent(class_="hatnote navigation-not-searchable")
+            and not link.find_parent('table') ]
         filteredHrefs = [href for href in hrefs if href and href.startswith('/wiki/')
-            and not href.startswith('/wiki/Wikipedia:') and not href.startswith('/wiki/Help:')
-            and not href.startswith('/wiki/File:')]
-
-        print(filteredHrefs)
-    
-
-
-    
-
-# def check_wiki_title(title):
-#     wikiTitles = []
-#     newTitle = get_wiki_title(title)
-#     if newTitle == 'Philosophy':
-#         return print("{number} roads from {title} to Philosophy".format(number=len(wikiTitles), title=wikiTitles[0] if len(wikiTitles) > 0 else 'Philosophy'))
-
-    
-
+            and ':' not in href
+            ]
+        return self.get_wiki_title(filteredHrefs[0])
 
 def main():
     if len(sys.argv) != 2:
         print("Need one argument")
         return
     test = roads_to_philosophy()
-    test.get_wiki_title(sys.argv[1])
+    test.get_wiki_title('/wiki/' + sys.argv[1])
 
 if __name__ == '__main__':
     main()
